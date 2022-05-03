91f8f3cf92b49132753bd91f2bb953dd6a6a88b9
abubjazov <abubjazov@mail.ru>
2022-05-04 00:08:14 +0300

    fixed unused code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,6 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
-// import { composeWithDevTools } from '@redux-devtools/extension'
 
 import { rootReducer } from './reducers'
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
-
-// export const store = createStore(
-// 	rootReducer,
-// 	composeWithDevTools(applyMiddleware(thunk))
-// )
